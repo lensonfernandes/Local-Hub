4c5a0661bb5d0a6a13fc462f60f226413b7b8978
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-06-15 10:17:51 +0530

    Added Content API to app

---
+++ local-hub/src/reducer.js
@@ -0,0 +1,47 @@
+// reducer.js
+
+// Declare initial state
+const initialState = {
+    basket: [], // Shopping basket details of the logged-in user
+    user: null, // Details of the logged-in user
+  };
+  
+  // Calculate the total amount of items in the basket
+  const calculateBasketTotal = (basket) => {
+    return basket.reduce((total, item) => total + item.price, 0);
+  };
+  
+  // Reducer function
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_TO_BASKET":
+        return {
+          ...state,
+          basket: [...state.basket, action.item],
+        };
+      case "REMOVE_FROM_BASKET":
+        const updatedBasket = state.basket.filter(
+          (item) => item.id !== action.id
+        );
+        return {
+          ...state,
+          basket: updatedBasket,
+        };
+      case "SET_USER":
+        return {
+          ...state,
+          user: action.user,
+        };
+      case "EMPTY_BASKET":
+        return {
+          ...state,
+          basket: [],
+        };
+      default:
+        return state;
+    }
+  };
+  
+  // Export the reducer
+  export { initialState, calculateBasketTotal, reducer };
+  
